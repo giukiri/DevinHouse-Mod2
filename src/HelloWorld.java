957776db98bca73b3a14e8e850732c9d359d61a6
Giulia Kirinus <giulia.kirinus@gmail.com>
2022-10-10 00:33:30 -0300

    2

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        //inicio do programa
+
+            int a = 1;
+            int b = 2;
+          var Resultado = a+b ;
+
+        System.out.println(Resultado);
+    }
+
+}
