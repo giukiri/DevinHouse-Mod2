eead5bd543194adbafe8ab0c7fac109dbaa290cd
Giulia Kirinus <giulia.kirinus@gmail.com>
2022-10-10 00:46:02 -0300

    ex1 M2S02

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        //inicio do programa
+
 
             int a = 1;
             int b = 2;
---
+++ src/Soma.java
@@ -0,0 +1,10 @@
+public class Soma {
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 2;
+       var Resultado = a+b ;
+
+        System.out.println(Resultado);
+    }
+}
