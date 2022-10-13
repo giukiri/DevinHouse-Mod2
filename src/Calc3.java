39a903267ad3a933e57d8463437d242ccc3cce15
Giulia Kirinus <giulia.kirinus@gmail.com>
2022-10-13 17:59:32 -0300

    ex4 M2S02

--- src/Calc3.java
+++ src/Calc3.java
@@ -1,14 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Calc3 {
     public static void main(String[] args) {
         Scanner valor = new Scanner(System.in);
 
+        List<Double> lista = new ArrayList<>();
+
         int option = 0 ;
 
         double num1;
         double num2;
-        boolean sair = false;
+        double resultado = 0;
 
         do {
 
@@ -31,20 +35,27 @@ public class Calc3 {
             num2 = valor.nextInt();
 
             if (option == 1) {
-                System.out.println("A soma é:  " + (num1 + num2));
+                resultado = num1+num2;
+                System.out.println("A soma é:  " + (resultado));
+
             } else if (option == 2) {
-                System.out.println("A subtração é: " + (num1 - num2));
+                resultado = num1 - num2;
+                System.out.println("A subtração é: " + (resultado));
             } else if (option == 3) {
-                System.out.println("A divisão é: " + (num1 / num2) + "e o resto da divisão é: " + (num1 % num2));
+                resultado = num1/num2;
+                System.out.println("A divisão é: " + (resultado));
             } else if (option == 4) {
-                System.out.println("A multiplicação é igual a : " + (num1 * num2));
+                resultado = num1 * num2;
+                System.out.println("A multiplicação é igual a : " + (resultado));
             }
 
-            if (option == 5) {
-                sair = true;
-            }
-        } while (sair != true);
+            lista.add(resultado);
+
+        } while (option != 5);
         System.out.println("FIM");
+
+
+        System.out.println("O histórico de resultados é : " + lista);
 
     }
 
