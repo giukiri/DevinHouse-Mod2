81839c97382efea915a455fd4ee1b644afd361f1
Giulia Kirinus <giulia.kirinus@gmail.com>
2022-10-15 03:13:47 -0300

    ex8 M2S02..

---
+++ src/Nomes3.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Nomes3 {
+    public static void main(String [] args){
+        Scanner entrada = new Scanner(System.in);
+
+        List<String> listaNomes  = new ArrayList<>();
+
+        int tamanho= 0;
+
+        String nome;
+
+       do {
+
+            System.out.println("Digite um nome!");
+            nome = entrada.next();
+
+            listaNomes.add(nome);
+
+            tamanho = listaNomes.size();
+
+        }
+        while(tamanho < 10);
+
+        try {
+
+            for (int i = 0; i < tamanho; i++) {
+                System.out.println((i + 1) + "=" + listaNomes);
+            }
+        } catch (NullPointerException e) {
+            throw new NullPointerException(null);
+        }
+    }
+}
+
