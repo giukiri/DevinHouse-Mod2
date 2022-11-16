5c95c8f512fe324be7895d86866f037820cc4ab4
Giulia Kirinus <giulia.kirinus@gmail.com>
2022-11-16 02:24:39 -0300

    Ex1 M2S04

---
+++ src/Semana4Exercicios/entites/Pessoa.java
@@ -0,0 +1,31 @@
+package Semana4Exercicios.entites;
+
+public class Pessoa {
+    private String name;
+    private Integer idade;
+
+    public Pessoa(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public Pessoa(String name, Integer idade){
+        this.idade=idade;
+        this.name=name;
+    }
+}
